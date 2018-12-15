97f842636c4bc997cc12a312e38728ea7aff9b0a
Muhammad Afzaal <afzaal@turnlunchon.com>
2018-12-15 20:06:56 +0500

    Export app as Static HTML

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/team': { page: '/team' }
+    }
+  }
+}
