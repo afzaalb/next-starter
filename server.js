6bf6530030bc391a6f77843623c0e286c5e956f7
Muhammad Afzaal <afzaal@turnlunchon.com>
2018-12-17 17:00:29 +0500

    Server js return fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000
 
 app.prepare()
 .then(() => {
@@ -12,16 +13,16 @@ app.prepare()
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
     const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
+    return app.render(req, res, actualPage, queryParams)
   })
 
   server.get('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
 .catch((ex) => {
