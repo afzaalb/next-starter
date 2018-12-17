a5cb86d5c5753ee0987ab0b2ce7169e3d8ccaa4a
Muhammad Afzaal <afzaal@turnlunchon.com>
2018-12-17 16:09:30 +0500

    Node server start command

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,47 @@
 import Layout from '../components/Layout';
-import Link from 'next/link';
+import PostLink from '../components/PostLink';
 import fetch from 'isomorphic-unfetch';
 import map from 'lodash/map'
-import capitalize from 'lodash/capitalize'
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?id=${props.id}`}>
-      <a>{capitalize(props.title)}</a>
-    </Link>
-  </li>
-)
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('constructor');
+  }
 
-const Index = props => (
-  <Layout>
-    <h2>Posts List</h2>
-    <ul>
-      {
-        map(props.posts, post => <PostLink key={post.id} id={post.id} title={post.title} body={post.body} />)
-      }
-    </ul>
-    <style jsx>{`
-      ul{
-        list-style-type: inside;
-        padding-left: 18px;
-        margin: 0;
-      }
-      h2{
-        margin: 0 0 15px;
-        line-height: 1.2;
-      }
-    `}</style>
-  </Layout>
-)
+  componentDidMount(){
+    console.log('componentDidMount');
+  }
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+  }
+  
+  render(){
+    console.log('render');
+    return(
+      <Layout>
+        <h2>Posts List</h2>
+        <ul>
+          {
+            map(this.props.posts, post => <PostLink key={post.id} id={post.id} title={post.title} body={post.body} />)
+          }
+        </ul>
+        <style jsx>{`
+          ul{
+            list-style-type: inside;
+            padding-left: 18px;
+            margin: 0;
+          }
+          h2{
+            margin: 0 0 15px;
+            line-height: 1.2;
+          }
+        `}</style>
+      </Layout>
+    )
+  }
+}
 
 Index.getInitialProps = async function() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
