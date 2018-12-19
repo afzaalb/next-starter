0d47d8e7011b5905fbbcba8685fbecf83b550018
Muhammad Afzaal <afzaalopera@gmail.com>
2018-12-19 19:05:47 +0500

    Static Assets served from static folder

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 const About = () => (
   <Layout>
     <h2>This is the about page</h2>
+    <img src="static/ticketmaster.jpg" style={{width:'100%'}} alt="Ticket Master" />
   </Layout>
 )
 
